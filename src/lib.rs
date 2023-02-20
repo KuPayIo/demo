fe6f23c546d2580d3ce2ae3992154ef1e7e8b3f0
lb <luob1215@gmail.com>
2023-02-20 16:13:25 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,9 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+    #[test]
+    fn it_works3() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
